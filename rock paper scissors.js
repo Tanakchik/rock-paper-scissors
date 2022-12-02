602012f1c3010482853f2e3930449f4bda4b5023
unclet <98801671+Tanakchik@users.noreply.github.com>
2022-12-02 11:57:10 +0200

    play round needs to prompt for every round

--- rock paper scissors.js
+++ rock paper scissors.js
@@ -2,26 +2,65 @@ function getComputerChoice(){
     return 'Rock','Paper','Scissors'
 }
 
-console.log(getComputerChoice)
+//console.log(getComputerChoice)
 
-function playRound( playerSelection, computerSelection){
-
-}
-
-function playRound(playerSelection, computerSelection){
+/*function playRound(playerSelection, computerSelection){
     if (playerSelection === 'rock','RocK','ROCK')
        (computerSelection === 'Paper')
     {
-        return "You Lose! Paper beats Rock";
+        console.log ("You Lose! Paper beats Rock");
+    
+    }  (playerSelection === 'scissors','Scissors','SCISSORS')
+       (computerSelection === 'Paper')
+    {
+        console.log ("You Lose! Paper beats scissors");
     }
+    
 }
-const playerSelection = "rock";
+/*const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));*/
 
-function game(){
-    for (let i = 0; i < 5; i++) {
-        
-     }
-// function inside a function 
+function game(){}
+let playerScore = 0;
+let computerScore= 0;
+let moves = 0;
+
+
+function playRound(){}
+
+ playerSelection = prompt("Choose one: rock,paper,or scissors"); 
+ computerSelection = getComputerChoice();
+
+ console.log(playerSelection)
+ console.log(computerSelection)
+
+ for (let i = 0; i < 5; i++) {
+    playRound();
+ 
+ }
+ if (playerSelection === computerSelection);{
+
+     alert ("Tie");
+ }
+ 
+ if  (playerSelection === 'rock','RocK','ROCK');{
+      (computerSelection === 'Paper');
+
+     alert ("You Lose! Paper beats Rock");
+
+} if (playerSelection === 'scissors','Scissors','SCISSORS');
+     (computerSelection === 'Paper');
+{
+     alert ("You Lose! Paper beats scissors");
 }
+if   (playerSelection === 'paper');
+     (computerSelection === 'Rock');
+{
+     alert ("You Win! Paper beats Computer");
+}
+if   (playerSelection === 'paper');{
+     (computerSelection === 'Scissors');
+}
+     alert ("You Win! Paper beats Computer");
+
