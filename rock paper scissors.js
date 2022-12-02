923720c1d6fa5fa1a17fbe0e1602d6afd8438bba
unclet <98801671+Tanakchik@users.noreply.github.com>
2022-12-02 11:55:02 +0200

    play round needs to figure how to prompt per round

--- rock paper scissors.js
+++ rock paper scissors.js
@@ -1,7 +1,27 @@
-let getComputerChoice = 'Rock' || 'Paper' || 'Scissors';
+function getComputerChoice(){
+    return 'Rock','Paper','Scissors'
+}
 
 console.log(getComputerChoice)
 
 function playRound( playerSelection, computerSelection){
 
+}
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === 'rock','RocK','ROCK')
+       (computerSelection === 'Paper')
+    {
+        return "You Lose! Paper beats Rock";
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        
+     }
+// function inside a function 
 }
